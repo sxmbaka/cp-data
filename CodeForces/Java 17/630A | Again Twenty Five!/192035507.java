bc7244ce986b374857dc12205a68bb4b21854047
Yatharth Singh <96312006+sxmbaka@users.noreply.github.com>
2023-02-19 04:05:03 +0530

    Committing files

---
+++ CodeForces/Java 17/630A | Again Twenty Five!/192035507.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class sol {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextLong();
+        System.out.println(25);
+    }
+}
