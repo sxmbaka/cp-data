270fe337652fc63a0aa849321e8bcbf05e52aabf
Yatharth Singh <96312006+sxmbaka@users.noreply.github.com>
2023-02-19 14:04:49 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/236A | Boy or Girl/194173110.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fr(s, e) for (int i = s; i <= e; i++)
+#define nl "\n"
+#define mod 1000000007
+void solve()
+{
+    string s;
+    cin >> s;
+    unordered_set<char> m;
+    for (auto i : s)
+        m.insert(i);
+    if (m.size() & 1)
+        cout << "IGNORE HIM!" << nl;
+    else
+        cout << "CHAT WITH HER!" << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+//#ifndef ONLINE_JUDGE
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+//#endif
+    ll test=1;
+    // cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
